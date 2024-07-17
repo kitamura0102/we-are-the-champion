e1e0ed0ced6f5929fdee5d2c79be9c3b3ced7d0a
kitamura0102 <raul_almonte001@hotmail.com>
2024-07-17 00:02:57 -0400

    Adding

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // javascript
+
+const publishBtn = document.getElementById("publish-btn")
+const endorsementsEl = document.getElementById("input")
+const fromEl = document.getElementById("from")
+const toEl = document.getElementById("to")
+let endorsementsDiv = document.getElementById("endorsements-para")
+
+publishBtn.addEventListener("click", function(){
+  createP()
+})
+function createP (){
+  let createdPara = document.createElement("p")
+  createdPara.textContent += endorsementsEl.value
+  
+  endorsementsDiv.appendChild(createdPara)
+  console.log(createdPara)
+  
+}
+
+console.log(endorsementsDiv)
