1eee182cbf0bd1bb32843d2314d8952bd8dfe29b
kitamura0102 <raul_almonte001@hotmail.com>
2024-07-19 00:11:46 -0400

    agregue el onvalue y el remove

--- index.js
+++ index.js
@@ -1,43 +1,72 @@
 // FireBase
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js"
-import { getDatabase,
-       ref,
-       push,
-       onValue} from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  push,
+  onValue,
+  remove,
+} from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js";
 
 const firebaseConfig = {
-  databaseURL: "https://realtime-database-c7a12-default-rtdb.firebaseio.com/"
-}
+  databaseURL: "https://realtime-database-c7a12-default-rtdb.firebaseio.com/",
+};
 
-const app = initializeApp(firebaseConfig)
-const database = getDatabase(app)
-const referenceInDb = ref(database, "endorsements")
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+const referenceInDb = ref(database, "endorsements");
 
-onValue(referenceInDb, function(snapshot){
-  let itemsArray = Object.entries(snapshot.val())
-  
-})
+onValue(referenceInDb, function (snapshot) {
+  const snapshotDoesExist = snapshot.exists();
+  if (snapshotDoesExist) {
+    let itemsArray = Object.values(snapshot.val());
+
+    appendItemToEndorsment(itemsArray);
+  } else {
+    endorsementsDiv.innerHTML = "No items yet.....";
+  }
+});
 
 // javascrip
 
-const publishBtn = document.getElementById("publish-btn")
-const endorsementsEl = document.getElementById("input")
-const fromEl = document.getElementById("from")
-const toEl = document.getElementById("to")
-let endorsementsDiv = document.getElementById("endorsements-para")
+const publishBtn = document.getElementById("publish-btn");
+const endorsementsEl = document.getElementById("input");
+const fromEl = document.getElementById("from");
+const toEl = document.getElementById("to");
+let endorsementsDiv = document.getElementById("endorsements-para");
 
-publishBtn.addEventListener("click", function(){
- 
-  createP()
-  push(referenceInDb, endorsementsEl.value)
-  endorsementsEl.value = ''
-   
-})
-function createP (){
-  let createdPara = document.createElement("p")
-  createdPara.textContent += endorsementsEl.value
-  
-  endorsementsDiv.appendChild(createdPara)
-  
+publishBtn.addEventListener("click", function () {
+  let inputValue = endorsementsEl.value;
+  let fromValue = fromEl.value;
+  let toValue = toEl.value;
+  push(referenceInDb, inputValue);
+  console.log(inputValue, fromValue, toValue);
+  clearInputValue();
+});
+
+function clearInputValue() {
+  endorsementsEl.value = "";
+  fromEl.value = "";
+  toEl.value = "";
 }
 
+function appendItemToEndorsment(item) {
+  let breakPoint = document.createElement("br")
+  let itemID = item[0];
+  let itemValue = item[1];
+  let createdParagraph = document.createElement("p");
+  
+
+  createdParagraph.textContent = `De: ${fromEl.value} 
+  ${endorsementsEl.value} 
+  Para: ${toEl.value}`;
+  endorsementsDiv.append(breakPoint,createdParagraph);
+console.log(endorsementsDiv)
+  createdParagraph.addEventListener("dblclick", function () {
+    // let exactLocationOfItemInDB = ref(database, `endorsements/`)
+    remove(referenceInDb);
+  });
+}
+function clearShoppingListEl() {
+  endorsementsDiv.innerHTML = "";
+}
